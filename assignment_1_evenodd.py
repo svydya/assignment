ff1b6daf83765711a18cc5e4f7d3d90f1d52a405
svydya <93927958+svydya@users.noreply.github.com>
2021-11-09 03:42:21 -0800

    Add files via upload

---
+++ assignment_1_evenodd.py
@@ -0,0 +1,12 @@
+numbers=(1,2,3,4,5,6,7,8,9)
+even_num=0
+odd_num=0
+for i in numbers:
+    if i%2:
+        even_num+=1
+    else:
+        odd_num+=1
+print("even numbers",even_num)
+print("odd numbers",odd_num)
+
+
